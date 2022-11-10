245d5cb90659c6e78867a1f7bd0e7ce0998ff575
{nmjuune} <{214002@himejo.jp}>
2022-11-10 11:21:36 +0900

    nmjuune

---
+++ js/main.js
@@ -0,0 +1,25 @@
+((d) => {
+    window.addEventListener('DOMContentLoaded', () => {
+
+        /**
+         * @type {
+         *      {
+         *          hamburgerTrigger: HTMLElement,
+         *          hamburgerHook: HTMLCollection,
+         *      }
+         * }
+         * 取得するDOM要素の格納オブジェクト
+         */
+        const $elms = {
+            hamburgerTrigger: d.getElementById('js-hamburger__trigger'),
+            hamburgerHook: d.getElementsByClassName('js-hamburger__hook')
+        };
+
+
+        $elms.hamburgerTrigger.addEventListener('click', (e) => {
+            Array.from($elms.hamburgerHook).forEach($elm => $elm.classList.toggle('ham-is-active'))
+        });
+
+
+    });
+})(document);
