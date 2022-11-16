d432a5184c3737b862868eb0f774048d8c92ca9e
{nmjuune} <{214002@himejo.jp}>
2022-11-16 10:53:20 +0900

    nmjuune

--- js/main.js
+++ js/main.js
@@ -4,64 +4,35 @@
 
 
 
-        let $header = new Ajax({
-            methodType: 'GET',
-            url: './header.html',
-            dataType: 'document',
-            timeout: 50
-        }).done(data => {
+        /**
+         * DOM要素を格納するオブジェクト
+         * @type {{
+         *      hamburgerTrigger:  HTMLElement,
+         *      hamburgerHook: HTMLCollectionOf<Element>
+         * }}
+         */
+        const $elms = {
+            hamburgerTrigger: d.getElementById('js-hamburger__trigger'),
+            hamburgerHook: d.getElementsByClassName('js-hamburger__hook')
+        };
 
-            d.body.prepend(data.body.children[0]);
+        /**
+         * ハンバーガーメニューのコンテンツを開閉する関数
+         * @param {MouseEvent} e addEventListenerのイベントオブジェクト
+         * @returns {void} 返り値なし
+         */
+        const hamburger = (e) => {
 
-            /**
-             * DOM要素を格納するオブジェクト
-             * @type {{
-             *      hamburgerTrigger:  HTMLElement,
-             *      hamburgerHook: HTMLCollectionOf<Element>
-             * }}
-             */
-            const $elms = {
-                hamburgerTrigger: d.getElementById('js-hamburger__trigger'),
-                hamburgerHook: d.getElementsByClassName('js-hamburger__hook')
-            };
+            const $target = e.currentTarget;
+            const $Array_elms = Array.from($elms.hamburgerHook);
 
-            /**
-             * ハンバーガーメニューのコンテンツを開閉する関数
-             * @param {MouseEvent} e addEventListenerのイベントオブジェクト
-             * @returns {void} 返り値なし
-             */
-            const hamburger = (e) => {
+            $target.classList.toggle('ham-is-active');
+            $Array_elms.forEach($elm => $elm.classList.toggle('ham-is-active'));
 
-                const $target = e.currentTarget;
-                const $Array_elms = Array.from($elms.hamburgerHook);
+        };
 
-                $target.classList.toggle('ham-is-active');
-                $Array_elms.forEach($elm => $elm.classList.toggle('ham-is-active'));
-
-            };
-
-            // ハンバーガーメニューを起動
-            $elms.hamburgerTrigger.addEventListener('click', (e) => hamburger(e));
-
-        }).fail(data => {
-            if (data) {
-                console.log(data);
-            }
-        });
-
-
-        let footer = new Ajax({
-            methodType: 'GET',
-            url: './footer.html',
-            dataType: 'document',
-            timeout: 50
-        })
-        .done(data => d.body.appendChild(data.body.children[0]))
-        .fail(data => {
-            if (data) {
-                console.log(data);
-            }
-        });
+        // ハンバーガーメニューを起動
+        $elms.hamburgerTrigger.addEventListener('click', (e) => hamburger(e));
 
 
 
