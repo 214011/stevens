1492db0fae44f1c3e8b075d1c2faadc96660e875
{nmjuune} <{214002@himejo.jp}>
2022-11-30 23:25:03 +0900

    nmjuune

--- js/contact.js
+++ js/contact.js
@@ -3,10 +3,48 @@
     w.addEventListener('DOMContentLoaded', () => {
 
         const $elms = {
-            formTel: function (i) {return d.getElementsByClassName('js-contact-table__tel')[i]}
+            contactTable__tel: d.getElementsByClassName('js-contact-table__tel')
         };
 
-        console.log($elms.formTel(0))
+        // イベント起動
+        Array.from($elms.contactTable__tel).forEach(($elm, i) => $elm.addEventListener('input', e => inputHandler(e, i)));
+
+        /**
+         * @type {string[]} 電話番号を格納＆管理する配列
+         */
+        let telNumber = ['', '', ''];
+
+        /**
+         * input要素に入力したイベントハンドラ
+         * @param {InputEvent} e イベント引数
+         * @param {number} i インデックス番号
+         */
+        const inputHandler = (e, i) => {
+
+            const $target = e.target;
+            const inputVal = e.data;
+            const inputType = e.inputType;
+
+            // telNumberに入った情報を入力していくので下の処理以外は基本的に入力させない
+            $target.value = '';
+
+            if (inputType === 'deleteContentBackward') {
+                telNumber[i] = telNumber[i].slice(0, -1);
+                $target.value = telNumber[i];
+            } else {
+                if (/[0-9]/.test(inputVal)) {
+                    if (telNumber[i].length < 4) {
+                        telNumber[i] += inputVal;
+                        $target.value = telNumber[i];
+                    } else {
+                        $target.value = telNumber[i];
+                    }
+                } else {
+                    $target.value = telNumber[i];
+                }
+            }
+
+        };
 
     });
 })(document, window);
