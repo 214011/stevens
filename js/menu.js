f19e3d14d1082c538d7da138707f7533cb0f3215
{nmjuune} <{214002@himejo.jp}>
2022-12-07 09:52:24 +0900

    nmjuune

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,27 @@
 ((d, w) => {
     w.addEventListener('DOMContentLoaded', () => {
 
+        /**
+         * 別ページからページ内リンクにアクセスしてきた時の処理（初期化）
+         */
+        const init = ((url) => {
+            console.log(url);
+            const fps = 60;
+
+            const loop = () => {
+                const loopId = setTimeout(() => requestAnimationFrame(loop), 1000 / fps);
+                console.log(loopId)
+                // if (loopId >= 15) {
+                //     clearTimeout(loopId)
+                // }
+                w.scroll({
+                    top: 0,
+                    behavior: 'auto'
+                });
+            };
+            requestAnimationFrame(loop);
+        })(location.href);
+
         let linkItem = [];
         Array.from(d.getElementsByClassName('menu__link-list--item')).forEach($elm => linkItem.push($elm.getElementsByClassName('btn')[0]));
 
@@ -14,13 +35,12 @@
             const $target = e.currentTarget;
             const $targetElm = d.getElementById($target.href.slice($target.href.indexOf('#') + 1, $target.href.length));
 
-            const rect = $targetElm.getBoundingClientRect().top;
+            const positionTop = $targetElm.getBoundingClientRect().top;
 
-            $targetElm.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-                inline: "center"
-            });
+            w.scroll({
+                top: positionTop - 150,
+                behavior: 'smooth'
+            })
 
         };
 
