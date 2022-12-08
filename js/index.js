689e5434cdcdabeb84b15f86e556cd1e192382bc
{nmjuune} <{214002@himejo.jp}>
2022-12-08 19:48:01 +0900

    nmjuune

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 ((d, w) => {
     w.addEventListener('DOMContentLoaded', () => {
 
-        // 'content__area--is-active'
 
         /**
          * DOM要素を格納するオブジェクト
@@ -20,47 +19,59 @@
             contentArea: function (i) {return this.indexContainer[i].getElementsByClassName('content__area')[0]}
         };
 
-        let viewIndex = 2;
-        let setPosition = 0;
 
+        // スクロールイベントを起動
         w.addEventListener('scroll', () => scrollHandler());
 
+
+        /**
+         * バックグラウンド画像をスクロール範囲によって変更する関数
+         * @param {number} i viewIndexの数値
+         */
+        const bgChange = (i) => {
+            let className = d.body.classList[1];
+            d.body.classList.remove(className);
+            const newClass = className.replace(className[className.length - 1], i);
+            d.body.classList.add(newClass);
+        };
+
+        /**
+         * @type {number} バックグラウンド画像を動的に変更するための変数（数値）
+         */
+        let viewIndex = 2;
+
+        /**
+         * @type {number} スクロールが上にされているか下にされているかを判定するための変数
+         */
+        let setPosition = 0;
+
+        /**
+         * スクロールイベントのイベントハンドラ
+         * @return {void}
+         */
         const scrollHandler = () => {
 
             if (setPosition < document.documentElement.scrollTop) {
                 if (w.scrollY >= (w.innerHeight * viewIndex) - (w.innerHeight / 3)) {
                     if (viewIndex < 6) {
                         if ($elms.contentArea(viewIndex)) $elms.contentArea(viewIndex).classList.add('content__area--is-active');
-                        let className = d.body.classList[1];
-                        d.body.classList.remove(className);
-                        const newClass = className.replace(className[className.length - 1], viewIndex);
-                        d.body.classList.add(newClass);
+                        bgChange(viewIndex);
                         viewIndex++;
                     } else {
                         if (viewIndex === 6){
                             $elms.contentArea(viewIndex).classList.add('content__area--is-active');
-                            let className = d.body.classList[1];
-                            d.body.classList.remove(className);
-                            const newClass = className.replace(className[className.length - 1], viewIndex);
-                            d.body.classList.add(newClass);
+                            bgChange(viewIndex);
                         }
                     }
                 }
             } else {
                 if (w.scrollY <= (w.innerHeight * viewIndex) + (w.innerHeight / 3)) {
                     if (viewIndex >= 2) {
-                        let className = d.body.classList[1];
-                        d.body.classList.remove(className);
-                        const newClass = className.replace(className[className.length - 1], viewIndex);
-                        d.body.classList.add(newClass);
+                        bgChange(viewIndex);
                         viewIndex--;
                     } else {
                         if (viewIndex === 1) {
-                            let className = d.body.classList[1];
-                            d.body.classList.remove(className);
-                            const newClass = className.replace(className[className.length - 1], viewIndex);
-                            d.body.classList.add(newClass);
-                            viewIndex = 2;
+                            bgChange(viewIndex);
                         }
                     }
                 }
