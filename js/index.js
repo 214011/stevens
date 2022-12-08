b27de84e7089301cbe58f84c72aaa4d40444e9e6
{nmjuune} <{214002@himejo.jp}>
2022-12-08 17:50:40 +0900

    nmjuune

--- js/index.js
+++ js/index.js
@@ -21,31 +21,53 @@
         };
 
         let viewIndex = 2;
+        let setPosition = 0;
 
         w.addEventListener('scroll', () => scrollHandler());
 
         const scrollHandler = () => {
-            // console.log(w.scrollY)
-            if (w.scrollY >= (w.innerHeight * viewIndex) - (w.innerHeight / 3)) {
-                if (viewIndex < 6) {
-                    $elms.contentArea(viewIndex).classList.add('content__area--is-active');
-                    console.log(viewIndex)
-                    let className = d.body.classList[1];
-                    d.body.classList.remove(className);
-                    console.log(className[className.length - 1])
-                    const newClass = className.replace(className[className.length - 1], viewIndex);
-                    d.body.classList.add(newClass)
-                    viewIndex++;
-                } else {
-                    if (viewIndex === 6){
+
+            if (setPosition < document.documentElement.scrollTop) {
+                if (w.scrollY >= (w.innerHeight * viewIndex) - (w.innerHeight / 3)) {
+                    if (viewIndex < 6) {
                         $elms.contentArea(viewIndex).classList.add('content__area--is-active');
                         let className = d.body.classList[1];
                         d.body.classList.remove(className);
                         const newClass = className.replace(className[className.length - 1], viewIndex);
-                        d.body.classList.add(newClass)
+                        d.body.classList.add(newClass);
+                        viewIndex++;
+                    } else {
+                        if (viewIndex === 6){
+                            $elms.contentArea(viewIndex).classList.add('content__area--is-active');
+                            let className = d.body.classList[1];
+                            d.body.classList.remove(className);
+                            const newClass = className.replace(className[className.length - 1], viewIndex);
+                            d.body.classList.add(newClass);
+                        }
+                    }
+                }
+            } else {
+                if (w.scrollY <= (w.innerHeight * viewIndex) + (w.innerHeight / 3)) {
+                    if (viewIndex >= 2) {
+                        let className = d.body.classList[1];
+                        d.body.classList.remove(className);
+                        const newClass = className.replace(className[className.length - 1], viewIndex);
+                        d.body.classList.add(newClass);
+                        viewIndex--;
+                    } else {
+                        if (viewIndex === 1) {
+                            let className = d.body.classList[1];
+                            d.body.classList.remove(className);
+                            const newClass = className.replace(className[className.length - 1], viewIndex);
+                            d.body.classList.add(newClass);
+                            viewIndex = 2;
+                        }
                     }
                 }
             }
+
+            setPosition = document.documentElement.scrollTop;
+
         };
 
 
