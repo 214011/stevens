05ced15042e155b4b34a4453bef5f55360c4802d
{kubataD} <{214007@himejo.jp}>
2022-12-09 09:33:51 +0900

    kubata

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 ((d, w) => {
     w.addEventListener('DOMContentLoaded', () => {
 
-        // 'content__area--is-active'
 
         /**
          * DOM要素を格納するオブジェクト
@@ -20,32 +19,67 @@
             contentArea: function (i) {return this.indexContainer[i].getElementsByClassName('content__area')[0]}
         };
 
-        let viewIndex = 2;
 
+        // スクロールイベントを起動
         w.addEventListener('scroll', () => scrollHandler());
 
+
+        /**
+         * バックグラウンド画像をスクロール範囲によって変更する関数
+         * @param {number} i viewIndexの数値
+         * @return {void} 返り値なし
+         */
+        const bgChange = (i) => {
+            let className = d.body.classList[1];
+            d.body.classList.remove(className);
+            const newClass = className.replace(className[className.length - 1], i);
+            d.body.classList.add(newClass);
+        };
+
+        /**
+         * @type {number} バックグラウンド画像を動的に変更するための変数（数値）
+         */
+        let viewIndex = 2;
+
+        /**
+         * @type {number} スクロールが上にされているか下にされているかを判定するための変数
+         */
+        let setPosition = 0;
+
+        /**
+         * スクロールイベントのイベントハンドラ
+         * @return {void} 返り値なし
+         */
         const scrollHandler = () => {
-            // console.log(w.scrollY)
-            if (w.scrollY >= (w.innerHeight * viewIndex) - (w.innerHeight / 3)) {
-                if (viewIndex < 6) {
-                    $elms.contentArea(viewIndex).classList.add('content__area--is-active');
-                    console.log(viewIndex)
-                    let className = d.body.classList[1];
-                    d.body.classList.remove(className);
-                    console.log(className[className.length - 1])
-                    const newClass = className.replace(className[className.length - 1], viewIndex);
-                    d.body.classList.add(newClass)
-                    viewIndex++;
-                } else {
-                    if (viewIndex === 6){
-                        $elms.contentArea(viewIndex).classList.add('content__area--is-active');
-                        let className = d.body.classList[1];
-                        d.body.classList.remove(className);
-                        const newClass = className.replace(className[className.length - 1], viewIndex);
-                        d.body.classList.add(newClass)
+
+            if (setPosition < document.documentElement.scrollTop) {
+                if (w.scrollY >= (w.innerHeight * viewIndex) - (w.innerHeight / 3)) {
+                    if (viewIndex < 6) {
+                        if ($elms.contentArea(viewIndex)) $elms.contentArea(viewIndex).classList.add('content__area--is-active');
+                        bgChange(viewIndex);
+                        viewIndex++;
+                    } else {
+                        if (viewIndex === 6){
+                            $elms.contentArea(viewIndex).classList.add('content__area--is-active');
+                            bgChange(viewIndex);
+                        }
+                    }
+                }
+            } else {
+                if (w.scrollY <= (w.innerHeight * viewIndex) + (w.innerHeight / 3)) {
+                    if (viewIndex >= 2) {
+                        bgChange(viewIndex);
+                        viewIndex--;
+                    } else {
+                        if (viewIndex === 1) {
+                            bgChange(viewIndex);
+                        }
                     }
                 }
             }
+
+            setPosition = document.documentElement.scrollTop;
+
         };
 
 
--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,101 @@
 ((d, w) => {
     w.addEventListener('DOMContentLoaded', () => {
 
-        let linkItem = [];
-        Array.from(d.getElementsByClassName('menu__link-list--item')).forEach($elm => linkItem.push($elm.getElementsByClassName('btn')[0]));
+        /**
+         * URLのIDから要素を取得する関数
+         * @param {string} url IDが含まれたURL（href属性）
+         * @return {HTMLElement}
+         */
+        const getElementByUrlId = (url) => d.getElementById(url.slice(url.indexOf('#') + 1, url.length));
 
-        linkItem.forEach($elm => $elm.addEventListener('click', e => clickHandler(e)));
+        /**
+         * 別ページからページ内リンクにアクセスしてきた時の処理（初期化）
+         * @param {string} url URLを取得
+         * @return {void} 返り値なし
+         */
+        const init = (url) => {
+            // もしurlに「 #(id) 」が含まれていたなら
+            if (url.includes('#')) {
 
+                /**
+                 * アニメーションのループ関数
+                 * @return {void} 返り値なし
+                 */
+                const loop = () => {
+
+                    // アニメーション呼び出し＆IDを定数に格納
+                    const loopId = requestAnimationFrame(loop);
+
+                    // もしloopIdが1以上
+                    if (loopId >= 1) {
+                        // アニメーションループ停止
+                        cancelAnimationFrame(loopId);
+
+                        // URLにリクエストされたIDを要素として取得
+                        const $urlElm = getElementByUrlId(url);
+                        // 取得した要素の絶対位置を取得
+                        const $positionTop = $urlElm.getBoundingClientRect().top;
+                        // 要素より-150pxした位置にスクロールさせる処理
+                        w.scroll(0, w.scrollY + $positionTop - 150);
+
+                    } else {
+                        // 上記以外はページのトップに固定
+                        w.scroll(0, 0);
+                    }
+
+                };
+
+                // 最初のアニメーション呼び出し
+                requestAnimationFrame(loop);
+
+            }
+        };
+        init(location.href);
+
+
+        /**
+         * ページ内リンクの要素を格納する配列を用意
+         * @type {HTMLElement[]}
+         */
+        let $linkItem = [];
+
+        // メインコンテンツのページ内リンクを取得＆$linkItemの配列に格納
+        Array.from(d.getElementsByClassName('menu__link-list--item')).forEach($elm => $linkItem.push($elm.getElementsByClassName('btn')[0]));
+        // フッターコンテンツのページ内リンクを取得＆$linkItemの配列に格納
+        Array.from(d.getElementById('menu__link-list').getElementsByTagName('li')).forEach($elm => $linkItem.push($elm.children[0]));
+
+        // 配列に格納されたページ内リンクの要素をまとめてクリックイベントを起動
+        $linkItem.forEach($elm => $elm.addEventListener('click', e => clickHandler(e)));
+
+        /**
+         * ページ内リンクをクリックした時のイベントハンドラ
+         * @param {MouseEvent} e クリックイベントのオブジェクト
+         * @return {void} 返り値なし
+         */
         const clickHandler = (e) => {
 
+            // リンクのジャンプを止める
             e.preventDefault();
 
+            // クリックイベントをまとめて起動しているのでクリックしているターゲットとなる要素を取得
             const $target = e.currentTarget;
-            const $targetElm = d.getElementById($target.href.slice($target.href.indexOf('#') + 1, $target.href.length));
+            const $targetElm = getElementByUrlId($target.href);
 
-            const rect = $targetElm.getBoundingClientRect().top;
+            // init関数内で記述した同じ定数名だが変数のスコープ（影響範囲）が別ブロックのため宣言できできる
+            const $positionTop = $targetElm.getBoundingClientRect().top;
 
-
-            $targetElm.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-                inline: "center"
-            });
+            w.scroll({
+                top: w.scrollY + $positionTop - 150,
+                behavior: 'smooth'
+            })
 
         };
+
+
+        const $toTop = d.getElementById('js-toTop');
+
+        $toTop.addEventListener('click', () => w.scroll({top: 0, behavior: 'smooth'}))
+
 
     });
 })(document, window);
