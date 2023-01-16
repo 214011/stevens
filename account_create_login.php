43ff130759cb8ffd67879965937ef84dd96258a6
{nmjuune} <{214002@himejo.jp}>
2023-01-17 01:16:09 +0900

    nmjuune

--- account_create_login.php
+++ account_create_login.php
@@ -3,6 +3,8 @@
     require_once('module/user.php');
     require_once('module/login.php');
     $user = unserialize($_COOKIE['user']);
-    $login = new Login($user->get_mailAddress(), $_GET['origin_pswd']);
-    header("Location: ./account_success.php");
+    $login = new Login($user->get_mailAddress(), $_GET['origin_pswd'], $user->get_password());
+    if ($login->is_pass()) {
+        header("Location: ./account_success.php");
+    }
 ?>
--- module/login.php
+++ module/login.php
@@ -1,5 +1,4 @@
 <?php
-    require_once('user.php');
     /**
      * ログイン管理をするクラス。
      */
@@ -7,31 +6,30 @@
 
         private string $mailAddress;
         private string $password;
-        private User $user;
+        private bool $pass_state;
 
         /**
          * コンストラクターで'login'のキー名で＄_SESSION変数にセットする。キーの値は、['mailAddress' => $val, 'password' => $val]
          * @param string $mailAddress メールアドレスの文字列
          * @param string $password パスワード文字列
          */
-        public function __construct (string $mailAddress, string $password) {
+        public function __construct (string $mailAddress, string $origin_password, string $hashed_password) {
             $this->mailAddress = $mailAddress;
-            $this->password = $password;
-            var_dump(self::is_user_cookie());
-            if (self::is_user_cookie()) {
-                $this->user = unserialize($_COOKIE['user']);
-                if ((password_verify($this->password, $this->user->get_password()))) {
-                    $this->set_session();
-                } else {
-                    // no process
-                }
+            $this->password = $origin_password;
+            if ((password_verify($this->password, $hashed_password))) {
+                $this->set_session();
+                $this->pass_state = true;
             } else {
-                unset($_SESSION['login']);
+                $this->pass_state = false;
             }
         }
 
         private function set_session () {
             $_SESSION['login'] = ['mailAddress' => $this->mailAddress, 'password' => $this->password];
+        }
+
+        public function is_pass () {
+            return $this->pass_state;
         }
 
         public static function is_user_cookie () {
