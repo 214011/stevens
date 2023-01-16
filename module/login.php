a4c236afa7c458448b04cbb2f090050594a5666a
{nmjuune} <{214002@himejo.jp}>
2023-01-17 01:28:04 +0900

    nmjuune

--- module/login.php
+++ module/login.php
@@ -11,35 +11,48 @@
         /**
          * コンストラクターで'login'のキー名で＄_SESSION変数にセットする。キーの値は、['mailAddress' => $val, 'password' => $val]
          * @param string $mailAddress メールアドレスの文字列
-         * @param string $password パスワード文字列
+         * @param string $origin_password 認証したいパスワードの文字列
+         * @param string $hashed_password ハッシュ化されたパスワードの文字列
          */
         public function __construct (string $mailAddress, string $origin_password, string $hashed_password) {
             $this->mailAddress = $mailAddress;
             $this->password = $origin_password;
             if ((password_verify($this->password, $hashed_password))) {
-                $this->set_session();
+                $this->set_login_session();
                 $this->pass_state = true;
             } else {
                 $this->pass_state = false;
             }
         }
 
-        private function set_session () {
+        /**
+         * 見やすいようにクラス内メソッドで処理をカプセル化しておく
+         * @return void
+         */
+        private function set_login_session () {
             $_SESSION['login'] = ['mailAddress' => $this->mailAddress, 'password' => $this->password];
         }
 
+        /**
+         * パスワードが認証できたかどうかを条件値で返すメソッド
+         * @return bool
+         */
         public function is_pass () {
             return $this->pass_state;
         }
 
-        public static function is_user_cookie () {
-            return isset($_COOKIE['user']);
-        }
-
+        /**
+         * ログインセッション変数を破棄する静的メソッド
+         * @return void
+         */
         public static function logout () {
             unset($_SESSION['login']);
         }
 
+        /**
+         * ログインセッション変数が存在しているかどうかを条件値で返す静的メソッド（ログインの有無）
+         * @return bool
+         */
         public static function is_login () {
             if (isset($_SESSION['login'])) {
                 return true;
