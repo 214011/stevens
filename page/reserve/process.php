4270c98cba43445ccde62f0c2e705487fc404da9
{nmjuune} <{214002@himejo.jp}>
2023-01-24 10:56:51 +0900

    nmjuune

--- page/reserve/process.php
+++ page/reserve/process.php
@@ -1,2 +1,19 @@
 <?php
-    echo $_GET['datetime'];
+    require_once('../../module/utility_functions.php');
+    get_class_login();
+    if (Login::is_login()) {
+        if (empty($_GET['datetime'])) {
+            header('Location: ./');
+        } else {
+            try {
+                $date = new DateTime($_GET['datetime']);
+            } catch (Exception $e) {
+                header('Location: ./');
+            }
+
+            echo $date->format('Y-m-d H:i:s');
+        }
+    } else {
+        header('Location: ./');
+    }
+?>
