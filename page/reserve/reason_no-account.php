c1d90eb2a161459f2a05392f64a5f216d6cfa678
{214011} <{214011@himejo.jp}>
2023-01-24 10:32:36 +0900

    nagai

---
+++ page/reserve/reason_no-account.php
@@ -0,0 +1,26 @@
+<?php require_once('../../module/utility_functions.php'); ?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>予約失敗｜Bordeaux</title>
+        <?php get_head(); ?>
+    </head>
+    <body>
+        <?php get_header(); ?>
+        <main class="main">
+            <h2 class="main--title main--title_reserve">
+                <span class="span-block"><i class="fa-regular fa-calendar-days"></i>Failed</span>
+                <span class="fa-sr-only">-</span>
+                <span class="span-block">アカウントを作成してください</span>
+            </h2>
+            <div class="account main__content content-w">
+                <div class="account--text content-w">
+                    <p>ア</p>
+                    <p class="btn__outer fx-jc-center"><a class="btn" href="process.php"><i class="fa-solid fa-right-from-bracket"></i></i>アカウントを作成/a></p>
+                </div>
+            </div>
+            </div>
+        </main>
+        <?php get_footer(); ?>
+    </body>
+</html>
