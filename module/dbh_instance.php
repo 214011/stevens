5ec11aff01e99053fabbe376e281ed4fc9f720ac
{nmjuune} <{214002@himejo.jp}>
2023-01-24 21:36:29 +0900

    nmjuune

--- module/dbh_instance.php
+++ module/dbh_instance.php
@@ -5,18 +5,3 @@
         $dbh->set_tableName('user_info');
         return $dbh;
     }
-
-
-    // $stmt = $dbh->query__UPDATE([
-    //     DBH::SQL__SET => [
-    //         ['username' => ':username'],
-    //         ['password' => ':password'],
-    //     ],
-    //     DBH::SQL__WHERE => ['id' => ':id']
-    // ]);
-    // $dbh->bindValue($stmt,[
-    //     [':username', '野村　純平', PDO::PARAM_STR],
-    //     [':password', 'jnpnmr1227', PDO::PARAM_STR],
-    //     [':id', 3, PDO::PARAM_INT]
-    // ]);
-    // $stmt->execute();
